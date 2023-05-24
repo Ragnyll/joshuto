9c8b435a1ba9999a2943b4a1b0c6b68f0e9770cb
ragnyll <ragnyll@gallowzhumour.dev>
2023-05-23 22:15:52 -0700

    now we're back in business

--- src/event/mod.rs
+++ src/event/mod.rs
@@ -1,10 +1,5 @@
 pub mod app_event;
 pub mod process_event;
-pub mod termion;
-//pub mod crossterm;
 
-pub use self::termion::*;
-//pub use self::crossterm::*;
+pub use self::app_event::*;
 pub use self::process_event::*;
-
-use std::sync::mpsc;
